6445fb1b0b513147f792ea94d6b3ec2c4e6f1a90
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-10-26 15:15:02 +0100

    Create better package structure

---
+++ clinstatsdb/clinstatsdb/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+from ..settings import BaseConfig 
+
+config = BaseConfig()
+db = declarative_base()
+
+engine = create_engine(config.SQLALCHEMY_DATABASE_URI)
+Session = sessionmaker()
+Session.configure(bind=engine)
+SQL = Session()
