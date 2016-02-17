0fc70bf02e33a33d49883be18d4a64b0dd07ee3f
Robin Andeer <robin.andeer@gmail.com>
2016-02-17 13:52:45 +0100

    extract session creation from module

---
+++ clinstatsdb/db/store.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+from .models import db
+
+
+def connect(connection_string):
+    """Connect to the database."""
+    engine = create_engine(connection_string)
+    db.metadata.bind = engine
+    session = scoped_session(sessionmaker(bind=engine))
+    db.query = session.query_property()
+    return session
