9517f5835b904b1f77dfcb931c22e868fc513d84
Kenny Billiau <kenny.billiau@scilifelab.se>
2016-10-03 13:48:53 +0200

    Bump version: 0.13.1 → 0.13.2

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='clinstatsdb',
-    version='0.13.1',
+    version='0.13.2',
     description='Models and access to clinstatsdb',
     author='Kenny Billiau',
     author_email='kenny.billiau@scilifelab.se',
