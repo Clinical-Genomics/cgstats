e271ddace4b2493e548b9ea43e739af1cf86ac65
Robin Andeer <robin.andeer@gmail.com>
2017-06-13 10:58:44 +0200

    bump version to 1.0 🎉

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     # Versions should comply with PEP440. For a discussion on
     # single-sourcing the version across setup.py and the project code,
     # see http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.14.5',
+    version='1.0.0',
     description='Models and access to cgstats',
     long_description=long_description,
     # What does your project relate to? Separate with spaces.
