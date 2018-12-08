4fa7c73641edd822dcbacecef4c2a823313567c2
Kenny Billiau <kenny.billiau@scilifelab.se>
2018-12-08 21:06:31 +0100

    Fix div by zero
    Fix non pooled lanes not getting stats. (there are never unpooled lanes.)

--- cgstats/db/novaseqparse.py
+++ cgstats/db/novaseqparse.py
@@ -308,24 +308,14 @@ def add(manager, demux_dir, unaligned_dir):
             u.sample_id = sample_id
             u.demux_id = demux_id
             u.lane = sample['Lane']
-            if nr_samples_lane[sample['Lane']] > 1:  # pooled!
-                stats_sample = stats_samples[sample['Lane']][sample['SampleID']]
-
-                u.yield_mb = round(int(stats_sample['pf_yield']) / 1000000, 2)
-                u.passed_filter_pct = stats_sample['pf_yield_pc']
-                u.readcounts = stats_sample['pf_clusters'] * 2
-                u.raw_clusters_per_lane_pct = stats_sample['raw_clusters_pc']
-                u.perfect_indexreads_pct = round(stats_sample['perfect_barcodes'] / stats_sample['barcodes'] * 100, 5)
-                u.q30_bases_pct = stats_sample['pf_Q30']
-                u.mean_quality_score = stats_sample['pf_qscore']
-            else:
-                u.yield_mb = round(int(stats[sample['Lane']]['pf_yield']) / 1000000, 2)
-                u.passed_filter_pct = stats[sample['Lane']]['pf_yield_pc']
-                u.readcounts = stats[sample['Lane']]['pf_clusters'] * 2
-                u.raw_clusters_per_lane_pct = stats[sample['Lane']]['raw_clusters_pc']
-                u.perfect_indexreads_pct = round(stats[sample['Lane']]['perfect_barcodes'] / stats[sample['Lane']]['barcodes'] * 100, 5)
-                u.q30_bases_pct = stats[sample['Lane']]['pf_Q30']
-                u.mean_quality_score = stats[sample['Lane']]['pf_qscore']
+            stats_sample = stats_samples[sample['Lane']][sample['SampleID']]
+            u.yield_mb = round(int(stats_sample['pf_yield']) / 1000000, 2)
+            u.passed_filter_pct = stats_sample['pf_yield_pc']
+            u.readcounts = stats_sample['pf_clusters'] * 2
+            u.raw_clusters_per_lane_pct = stats_sample['raw_clusters_pc']
+            u.perfect_indexreads_pct = round(stats_sample['perfect_barcodes'] / stats_sample['barcodes'] * 100, 5) if stats_sample['barcodes'] else 0
+            u.q30_bases_pct = stats_sample['pf_Q30']
+            u.mean_quality_score = stats_sample['pf_qscore']
             u.time = func.now()
 
             manager.add(u)
